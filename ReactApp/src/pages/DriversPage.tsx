d89819caada5ffbe64ead6311b5c03dc84334c78
Ahrow <jonasbergenheim90@gmail.com>
2023-11-07 15:19:24 +0100

    add showList driverspage + dependecies + debugs

--- ReactApp/src/pages/DriversPage.tsx
+++ ReactApp/src/pages/DriversPage.tsx
@@ -1,7 +1,10 @@
+import { DriverList } from "../components/DriverList";
+
 export const DriversPage = () => {
   return (
     <div>
       <h1>Drivers Page</h1>
+      <DriverList />
     </div>
   );
 };
