3aab11691cc0737fddb48a3162186e4211db1a45
Ahrow <jonasbergenheim90@gmail.com>
2023-11-07 15:16:39 +0100

    add cors

--- F1API/Program.cs
+++ F1API/Program.cs
@@ -6,13 +6,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<F1APIContext>(options => options.UseSqlite("Data Source=Databases/F1API.db"));
 
+builder.Services.AddCors(
+    options => {
+        options.AddPolicy("AllowAll",
+        policies => policies
+            .AllowAnyMethod()
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+        );
+    }
+);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 app.UseStaticFiles();
+app.UseCors("AllowAll");
 
 
 // Configure the HTTP request pipeline.
