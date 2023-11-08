4fcc3ff07bc2b4e9fd4516eaaf79e9f74abdd73f
Ahrow <jonasbergenheim90@gmail.com>
2023-11-08 21:06:24 +0100

    rm redudant files

--- ReactApp/src/components/crud-menu.tsx
+++
@@ -1,3 +0,0 @@
-export const CrudMenu = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex justify-evenly items-center">{children}</div>;
-};
--- ReactApp/src/components/post-driver.tsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { DriverCardProps } from "./cards";
-const driverEndpoint = "http://localhost:5173/drivers";
-
-export const PostDriver = async (newDriver: DriverCardProps) => {
-  const response = await axios.post(driverEndpoint, newDriver);
-  return console.log(response);
-};
