8d1adf7a8641b532317e05cee23cd2ebc9cbbe4e
Ahrow <jonasbergenheim90@gmail.com>
2023-11-23 20:20:09 +0100

    finish

--- ReactApp/src/components/car-item.tsx
+++ ReactApp/src/components/car-item.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import CarService from "../services/CarService";
 import { CarCard } from "./ui/cards";
 
